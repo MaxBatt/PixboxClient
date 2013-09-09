e3ece3e96b1c43784686252b3effb0b99abf568a
Max Batt <mail@maxbatt.de>
2013-09-09 18:49:53 +0200

    del

--- gen/de/pixbox/client/R.java
+++ gen/de/pixbox/client/R.java
@@ -19,17 +19,16 @@ public final class R {
         public static final int activity_horizontal_margin=0x7f040000;
     }
     public static final class drawable {
-        public static final int border=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int my_pixbox=0x7f020002;
-        public static final int my_pixbox_pressed=0x7f020003;
-        public static final int pixbox_button=0x7f020004;
-        public static final int take_picture=0x7f020005;
-        public static final int take_picture_button=0x7f020006;
-        public static final int take_picture_pressed=0x7f020007;
-        public static final int upload=0x7f020008;
-        public static final int upload_button=0x7f020009;
-        public static final int upload_pressed=0x7f02000a;
+        public static final int ic_launcher=0x7f020000;
+        public static final int my_pixbox=0x7f020001;
+        public static final int my_pixbox_pressed=0x7f020002;
+        public static final int pixbox_button=0x7f020003;
+        public static final int take_picture=0x7f020004;
+        public static final int take_picture_button=0x7f020005;
+        public static final int take_picture_pressed=0x7f020006;
+        public static final int upload=0x7f020007;
+        public static final int upload_button=0x7f020008;
+        public static final int upload_pressed=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f080010;
