33e8fd5f7f30c1aee19477f639a0617bb2e6020b
Max Batt <mail@maxbatt.de>
2013-09-16 21:24:52 +0200

    r

--- bin/MainActivity-debug-unaligned.apk.d
+++
@@ -1,7 +0,0 @@
-/Users/beatmax/Documents/Eclipse Workspaces/PixboxClient/bin/MainActivity-debug-unaligned.apk : \
-/Users/beatmax/Documents/Eclipse Workspaces/PixboxClient/bin/MainActivity.ap_ \
-/Users/beatmax/Documents/Eclipse Workspaces/PixboxClient/bin/classes.dex \
-/Users/beatmax/Documents/Eclipse Workspaces/PixboxClient/libs/gson-2.2.2.jar \
-/Users/beatmax/Documents/Eclipse Workspaces/PixboxClient/libs/android-smart-image-view-1.0.0.jar \
-/Users/beatmax/Documents/Eclipse Workspaces/PixboxClient/libs/android-async-http-1.4.3.jar \
-/Users/beatmax/Documents/Eclipse Workspaces/PixboxClient/libs/android-support-v4.jar \
